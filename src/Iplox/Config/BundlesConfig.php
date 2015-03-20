abcdfe719c8f4f50c1770b667c6422690edd24b9
Jr. Stanley Zapata <jrszapata@hotmail.com>
2015-03-20 15:42:01 -0400

    Config - Disabled Smarty and PhpActiveRecord bundles by default.

--- src/Iplox/Config/BundlesConfig.php
+++ src/Iplox/Config/BundlesConfig.php
@@ -4,9 +4,9 @@ namespace Iplox\Config;
 
 class BundlesConfig {
     //This could be Twig or any other php template engine adapted to work here.
-    const Smarty = 1;
+//    const Smarty = 1;
     
     //This could be Doctrine, Propel, RedBean or any other php ORM 
-    const PhpActiveRecord = 2;
+//    const PhpActiveRecord = 2;
     
 }
