8dad722a4e32bcaa799f6bddf77225cc69181a5f
Jr. Stanley Zapata <jzapata@stanlix.com>
2015-06-11 00:34:57 +0000

    HttpStatus - Added class for statuscodes

---
+++ src/Iplox/HttpStatus.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Iplox;
+
+class HttpStatus {
+    const OK = 200;
+    const CREATED = 201;
+    const BAD_REQUEST = 401;
+    const NOT_FOUND = 404;
+    const SERVER_ERROR = 500;
+}
