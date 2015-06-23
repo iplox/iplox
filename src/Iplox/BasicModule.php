015f7c1e0a6e17ccfd67be2e33c024a15dc57c3b
Jr. Stanley Zapata <jrszapata@hotmail.com>
2015-06-23 16:00:54 -0400

    Module - Added default configurations by calling addKnownOptions of the config property.

--- src/Iplox/BasicModule.php
+++ src/Iplox/BasicModule.php
@@ -2,14 +2,40 @@
 
 namespace Iplox;
 
+use Composer\Autoload\ClassLoader;
+
 class BasicModule extends ModuleAbstract {
 
     public function __construct($cfg)
     {
         parent::__construct($cfg);
+
+        //Add options for a General set.
+        $this->config->addKnownOptions([
+            // Submodules options
+            'modules' => [],
+            'modulesDir' => 'Modules',
+            'modulesClassName' => 'Module',
+
+            // Reserved options for "future" use
+            'bundlesDir' => 'Bundles',
+        ]);
+
+        //Add options for a Db (database) set.
+        $this->config->addKnownOptions('Db', [
+            'provider' => 'mysql',
+            'username' => 'root',
+            'password' => '',
+            'hostname' => 'localhost',
+            'port' => '3386',
+            'dbname' => 'IploxApp',
+        ]);
+
+        // Load the module routes.
         $this->addModuleRoutes();
     }
 
+    // If it has submodules, add the routes.
     protected function addModuleRoutes()
     {
         $modCfg = [];
@@ -23,6 +49,7 @@ class BasicModule extends ModuleAbstract {
         $this->router->appendRoutes($routes);
     }
 
+    //Initialize the module
     public function init($uri)
     {
         // Let's do it!
@@ -40,20 +67,27 @@ class BasicModule extends ModuleAbstract {
         $reqUri = preg_replace($regExpReq, '/', $this->router->request);
 
         if(array_key_exists('directory', $subModCfg) && array_key_exists('namespace', $subModCfg)) {
+            $loader = new ClassLoader();
             $loader->add($subModCfg['namespace'], $subModCfg['directory']);
             $loader->register();
             $loader->setUseIncludePath(true);
 
             if(array_key_exists('className', $subModCfg)){
-                $mClass = $subModCfg['namespace'].'\\'.$subModCfg['class'];
+                $mClass = $subModCfg['namespace'].'\\'.$subModCfg['className'];
             } else {
                 $mClass = $subModCfg['namespace'].'\\'.$this->config->modulesClassName;
             }
 
-            $mod = new $mClass(new Config($subModCfg));
+            $cfg = new Config(
+                $subModCfg['directory'],
+                array_key_exists('namespace', $subModCfg) ? $subModCfg['namespace'] : '',
+                array_key_exists('options', $subModCfg) ? $subModCfg['options'] : [],
+                $this->config->env
+            );
+            $mod = new $mClass($cfg);
             $mod->init($reqUri);
         } else {
-            throw new \Exception("The 'namespace' and/or 'directory' configuration options ".$subModCfg['name']." where not found for the ".$subModCfg['name'." module."]);
+            throw new \Exception("The 'namespace' and/or 'directory' configuration options ".$subModCfg['name']." where not found for the ".$subModCfg['name']." module.");
         }
     }
 }
